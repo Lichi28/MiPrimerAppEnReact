3cfb79de7379cb56a4909926ff9a03d190baa64a
Lizbeth <lizmury@hotmail.com>
2021-09-26 13:21:05 -0500

    A-003 Añadir estado a la aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {}
 
 export const App = (props: Props) => {
+  const [contador, setContador] = useState(0);
+  
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  };
+
   return (
     <div
       style={{
@@ -13,10 +23,14 @@ export const App = (props: Props) => {
         flexDirection: "column",
       }}
     >
-      <text style={{ fontSize: 200, color: "gray" }}>0</text>
+      <text style={{ fontSize: 200, color: "gray" }}>{contador}</text>
       <div style={{ flexGrow: 1, justifyContent: "space-between" }}>
-        <button style={{ height: 44, margin: 8 }}>Incrementar</button>
-        <button style={{ height: 44, margin: 8 }}>Decrementar</button>
+        <button onClick={incrementar} style={{ height: 44, margin: 8 }}>
+          Incrementar
+        </button>
+        <button onClick={decrementar} style={{ height: 44, margin: 8 }}>
+          Decrementar
+        </button>
       </div>
     </div>
   );
